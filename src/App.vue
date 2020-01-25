c33b565fa08ab2a5f9e06d63782b16ce2fc5600d
Mary Pirogova <marifry@yandex.ru>
2020-01-25 13:36:09 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,97 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="pagehead">
+        <h1 class="">Рейтинг участников</h1>
+        <div class="search-holder">
+          <input v-model="search" class="search-holder__input" />
+          <img
+            class="search-holder__icon icon icon--action"
+            src="./assets/search.svg"
+          />
+        </div>
+      </div>
+      
+      <div>
+<!--        <p @click="sortUsers">sort {{ sortAsc }}</p>-->
+        <user v-for="(user, index) in users" :key="user.index" :user="user" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import User from "./components/User.vue";
+import Service from "./services/userService";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    User
+  },
+  data() {
+    return {
+      search: "",
+      sortAsc: "",
+      service: new Service(),
+      users: []
+    };
+  },
+  computed: {
+    // users(){
+    //   return this.service.getUsers()
+    // }
+  },
+  watch: {
+    search(value) {
+      this.users = this.service.findUser(value);
+    },
+    sortAsc(value) {
+      this.users = this.service.sortUsersByRate(value);
+    }
+  },
+  created() {
+    this.users = this.service.getUsers();
+  },
+  methods: {
+    sortUsers() {
+      this.sortAsc = !this.sortAsc;
+    }
   }
-}
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.pagehead {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.userlist {
+ // padding: 35px 60px;
+}
+.search-holder {
+  position: relative;
+  &__input {
+    width: 250px;
+    height: 100%;
+    padding: 10px 35px 3px 0;
+    font-size: 1rem;
+    box-sizing: border-box;
+    border: none;
+    background: transparent;
+    border-bottom: solid 1px $l-gray;
+    &:focus {
+      outline: none;
+      border-color: $green;
+    }
+  }
+  &__icon {
+    position: absolute;
+    width: 24px;
+    height: 24px;
+    top: calc(50% - 12px);
+    right: 5px;
+  }
 }
 </style>
---
+++ src/components/User.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="card row">
+    <div class="col-10">
+      <div class="row">
+        <span class="col-1">
+          1
+        </span>
+        <div class="col-2">
+          <span>{{ getFirstLetter(user.name) }} {{ getFirstLetter(user.secondName) }}</span>
+        </div>
+        <div class="col-9">
+          <h5>{{ user.name }} {{ user.secondName }}</h5>
+          <!--				<p>{{user.secondName}}</p>-->
+        </div>
+      </div>
+    </div>
+
+    <div class="col-2">
+      <div>
+        <p>
+          <strong>{{ user.rating }}</strong>
+        </p>
+        <p>{{ getDeclOfNum(user.rating) }}</p>
+      </div>
+    </div>
+	  
+  </div>
+</template>
+
+<script>
+export default {
+  name: "User",
+  props: {
+    user: {
+      type: Object,
+      default: () => {},
+      required: true
+    }
+  },
+  data() {
+    return {
+      data1: null
+    };
+  },
+  computed: {
+    computed1() {}
+  },
+  methods: {
+    getDeclOfNum(num) {
+      let titles = ["балл", "балла", "баллов"];
+      return titles[
+        num % 10 === 1 && num % 100 !== 11
+          ? 0
+          : num % 10 >= 2 &&
+            num % 10 <= 4 &&
+            (num % 100 < 10 || num % 100 >= 20)
+          ? 1
+          : 2
+      ];
+    },
+    getFirstLetter(str) {
+      return str.charAt(0);
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.card {
+  /*display: flex;*/
+  /*align-items: center;*/
+  /*justify-content: space-between;*/
+}
+</style>
